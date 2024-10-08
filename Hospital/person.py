044b39bf2baca1969ae687a00da4c5eaef68bea4
BenjaminAston <aston.benjamin@gmail.com>
2024-10-08 17:26:18 +0200

    Create person.py

---
+++ Hospital/person.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+class Person:
+    def __init__(self, name: str, age: int, role: str):
+        self.name = name
+        self.age = age
+        self.role = role
+    @abstractmethod
+    def perform_task(self):
+        pass  # This is abstract, subclasses should override this
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_age(self) -> int:
+        return self.age
+
+    def get_title(self) -> str:
+        return self.role
+
+    def get_details(self) -> dict:
+        return {
+            "name": self.name,
+            "age": self.age,
+            "role": self.role
+        }
