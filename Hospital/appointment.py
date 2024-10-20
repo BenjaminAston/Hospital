8cadfbc5c8de295a81b728c4e69890dd4878a7f0
BenjaminAston <aston.benjamin@gmail.com>
2024-10-20 16:38:36 +0200

    Update appointment.py

--- Hospital/appointment.py
+++ Hospital/appointment.py
@@ -1,11 +1,17 @@
 from patient import Patient
 from doctor import Doctor
 
+
 class Appointment:
-    def __init__(self, doctor, patient, date):
+    next_id = 1
+    def __init__(self, doctor, patient, date, appointment_name):
         self.doctor = doctor
         self.patient = patient
         self.date = date
+        self.appointment_id = Appointment.next_id
+        Appointment.next_id += 1
+        self.appointment_name = appointment_name
 
     def __str__(self):
-        return f"Patient: {self.patient.get_name()}, Doctor: Dr. {self.doctor.get_name()}, Date: {self.date}"
+        return (f"Appointment ID: {self.appointment_id}, Appointment Name: {self.appointment_name}, "
+                f"Patient: {self.patient.get_name()}, Doctor: Dr. {self.doctor.get_name()}, Date: {self.date}")
